41e7ee8bc2317ed702508b31bfd548264f34aff6
Edward Kopp <edward@edwardkopp.com>
2026-05-12 22:29:31 -0400

    set title for error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,6 +3,10 @@
   import Link from "$lib/components/Link.svelte";
 </script>
 
+<svelte:head>
+  <title>{`${page.error?.message} - Edward Kopp`}</title>
+</svelte:head>
+
 <div style="text-align: center;">
   <h1>{page.status} &mdash; {page.error?.message}</h1>
   <p>Go to the <Link href="/">home page</Link>?</p>
